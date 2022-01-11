3a6f5e89e335b1d4bfed415ba0b33f516c196987
Ishu-Agarwal <2019ucp1379@mnit.ac.in>
2022-01-11 18:35:54 +0530

    corrected1

--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Book from './Book';
-import History from './History';
 import reportWebVitals from './reportWebVitals';
 ReactDOM.render(
   <React.StrictMode>
