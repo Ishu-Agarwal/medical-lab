a673c532d61c7c2bcad30607efa1972d71b8ff8d
Ishu-Agarwal <2019ucp1379@mnit.ac.in>
2022-01-12 09:46:24 +0530

    corrected15

--- db/conn.js
+++ db/conn.js
@@ -1,15 +1,15 @@
 const mongoose = require('mongoose');
 const db = process.env.DATABASE;
     // Connect to the MongoDB cluster
-  //    const connect=mongoose.connect(db,{
-  //       useNewUrlParser: true,
-  //       useCreateIndex:true,
-  //        useUnifiedTopology: true,
-  //        useFindAndModify:false 
-  //       },() => console.log("Mongoose is connnected") );
-  // } catch (e) {
-  //   console.log("could not connect");
-  // }
+    mongoose.connect(db,{
+        useNewUrlParser: true,
+        useCreateIndex:true,
+         useUnifiedTopology: true,
+         useFindAndModify:false 
+        },() => console.log("Mongoose is connnected") );
+  } catch (e) {
+    console.log("could not connect");
+  }
 //   const connect = mongoose.connect(db,{
 //            useNewUrlParser: true,
 //         useCreateIndex:true,
@@ -21,20 +21,20 @@ const db = process.env.DATABASE;
 // }, (err) => {
 //         console.log(err);
 // })
-const connectDB = async () => {
-	try {
-		await mongoose.connect(db
-      {
-                   useNewUrlParser: true,
-        useCreateIndex:true,
-         useUnifiedTopology: true,
-         useFindAndModify:false 
-      });
-		console.log("connected");
-	} catch (err) {
-		console.error(err.message);
-		// Exit process with failure
-		process.exit(1);
-	}
-};
-connectDB();
+// const connectDB = async () => {
+// 	try {
+// 		await mongoose.connect(db,
+//       {
+//                    useNewUrlParser: true,
+//         useCreateIndex:true,
+//          useUnifiedTopology: true,
+//          useFindAndModify:false 
+//       });
+// 		console.log("connected");
+// 	} catch (err) {
+// 		console.error(err.message);
+// 		// Exit process with failure
+// 		process.exit(1);
+// 	}
+// };
+// connectDB();
